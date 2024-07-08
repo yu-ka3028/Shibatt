654bbf95be3eb3a836bf34fa855ec6316cee8fc0
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-07-08 22:58:14 +0900

    sorcery

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+end
