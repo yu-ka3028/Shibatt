b003cad9ff97c018d6c543acc7fb19de76df7366
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-07 23:30:32 +0900

    login_5

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -18,13 +18,4 @@ Rails.application.config.sorcery.configure do |config|
   config.user_config do |user|
     user.authentications_class = Authentication
   end
-  
-  config.line.authorize_url = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&scope=profile&client_id=' + config.line.key + '&redirect_uri=' + config.line.callback_url + '&state=' + SecureRandom.hex(8) + '&display=page'
-  
-  # 開発環境
-  config.line.callback_url = 'https://shibatt-dcf5dffc0d02.herokuapp.com/oauth/callback?provider=line'
-
-  # テスト環境
-  # config.line.callback_url = 'http://localhost:3000/oauth/callback?provider=line'
-  
 end
