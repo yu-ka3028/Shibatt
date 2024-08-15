065b0d544914482ee0b1f32aa7587fd740748bb3
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-15 19:37:48 +0900

    line_pro_or_dev

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -6,12 +6,18 @@ Rails.application.config.sorcery.configure do |config|
   config.line.secret = Rails.application.credentials.dig(:line, :channel_secret)
 
   # 環境に応じてcallback_urlを設定
-  if Rails.env.production?
-    config.line.callback_url = "https://shibatt-dcf5dffc0d02.herokuapp.com/oauth/callback?provider=line"
-  else
-    config.line.callback_url = "http://localhost:3000/oauth/callback?provider=line"
-  end
+  # if Rails.env.production?
+  #   config.line.callback_url = "https://shibatt-dcf5dffc0d02.herokuapp.com/oauth/callback?provider=line"
+  # else
+  #   config.line.callback_url = "http://localhost:3000/oauth/callback?provider=line"
+  # end
 
+  if Rails.env.production?
+    config.line.callback_url = Rails.application.credentials.dig(:production, :line, :callback_url)
+  else
+    config.line.callback_url = Rails.application.credentials.dig(:development, :line, :callback_url)
+  end
+  
   config.user_config do |user|
     user.authentications_class = Authentication
     user.username_attribute_names = [:username]
