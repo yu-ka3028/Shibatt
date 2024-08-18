c1a7ecbe289562343c4b3eb9dfe0dd3236e6a7ba
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-18 23:50:35 +0900

    login?

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,11 +8,7 @@ class UserSessionsController < ApplicationController
       redirect_back_or_to root_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
-<<<<<<< Updated upstream
-      render :new
-=======
       render :login_path, status: :unauthorized
->>>>>>> Stashed changes
     end
   end
 
