a8b1c15d7177d2c1dae6d8884b282a66231ad5d7
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-20 16:39:45 +0900

    memo_create_from_line

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,23 +3,39 @@ class LinebotController < ApplicationController
   skip_before_action :require_login, only: :callback
   
   def callback
-    puts body = request.body.read
+    body = request.body.read
     events = client.parse_events_from(body)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          line_user_id = event['source']['userId']
+          user = User.find_by(line_user_id: line_user_id)
 
-          user = User.find_by(line_user_id: event['source']['userId'])
           if user
+            # ユーザーが登録されている場合、メモを保存
             user.memos.create(content: event.message['text'])
+            message = {
+              type: 'text',
+              text: "メモを保存しました: #{event.message['text']}"
+            }
+          else
+            # ユーザーが未登録の場合、新規登録を促すメッセージを送信
+            message = {
+              type: 'text',
+              text: "あなたはまだ登録されていません。以下のURLから新規登録してください。\n#{new_user_url}"
+            }
           end
+
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          client.reply_message(event['replyToken'], message)
+          # if user
+          #   user.memos.create(content: event.message['text'])
+          # end
         end
       end
     end
