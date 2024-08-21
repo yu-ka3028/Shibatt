d4be0d851b38176ce47b551fa89a035012a4cc4a
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-21 18:56:36 +0900

    deback

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   def top 
-    Rails.logger.info "Username: #{session[:username]}" # デバッグ用
+    Rails.logger.info "Username: #{session[:username]}"
     @user = current_user
     @memo = @user.memos.build if @user
     @memo_tags = @user.memos.flat_map(&:tags).uniq if @user
