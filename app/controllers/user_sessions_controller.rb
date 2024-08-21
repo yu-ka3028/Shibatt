6709177cd5b3b25aa5c2d610aad8d6446c856d0f
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-21 09:58:40 +0900

    flash_erroe_message

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
       redirect_back_or_to root_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
-      render :login_path, status: :unauthorized
+      render :new, status: :unauthorized
     end
   end
 
