90ed497063d63f08438f1ed5c9291a07516b1e37
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-22 23:35:08 +0900

    memo/ref_memo

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -6,7 +6,7 @@ class Memo < ApplicationRecord
   has_many :reflection_memo_memos
   has_many :reflection_memos, through: :reflection_memo_memos
 
-  validates :content, presence: true, length: { maximum: 5 }
+  validates :content, presence: true, length: { maximum: 255 }
 
   def self.ransackable_attributes(auth_object = nil)
     ["content", "created_at", "progress"]
--- app/models/reflection_memo.rb
+++ app/models/reflection_memo.rb
@@ -3,7 +3,7 @@ class ReflectionMemo < ApplicationRecord
   has_many :reflection_memo_memos
   has_many :memos, through: :reflection_memo_memos
 
-  validates :content, presence: true, length: { maximum: 5 }
+  validates :content, presence: true, length: { maximum: 255 }
   after_save :update_memo_progress
 
   def self.ransackable_attributes(auth_object = nil)
