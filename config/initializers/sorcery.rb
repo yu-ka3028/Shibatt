952a9f88d6d2b59e445161e1265aefa6ad227ee4
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-24 16:48:05 +0900

    refac_env

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -1,19 +1,17 @@
 Rails.application.config.sorcery.submodules = [:external]
 Rails.application.config.sorcery.configure do |config|
+  #(本リリースで外すか検討中)Sorceryの内部認証を使用
   config.user_class = "User"
   config.user_config do |user|
     user.authentications_class = Authentication
+    #ユーザー名をデフォルトのemailからusernameに変更
     user.username_attribute_names = [:username]
   end
 
   config.external_providers = [:line]
   config.line.key = Rails.application.credentials.dig(:line, :channel_id)
-  config.line.secret = Rails.application.credentials.dig(:line, :channel_secret)
-  # if Rails.env.production?
-  #   config.line.callback_url = Rails.application.credentials.dig(:production, :line, :callback_url)
-  # else
-  #   config.line.callback_url = Rails.application.credentials.dig(:development, :line, :callback_url)
-  # end  
+  config.line.secret = Rails.application.credentials.dig(:line, :channel_secret) 
   config.line.callback_url = Rails.application.credentials.dig(Rails.env, :line, :callback_url)
+  #LINEから取得したdisplayNameをローカルでusernameとして使用
   config.line.user_info_mapping = { username: 'displayName' }
 end
