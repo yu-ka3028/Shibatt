ec2d0940c701958b1bb2e7503d7eeaa7e7a8e701
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-08-28 17:26:13 +0900

    shiba_FB_prompt

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join('app/services')
   end
 end
