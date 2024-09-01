f3b56f880cb8719897f0da1ffb9fbd291a80b264
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-01 18:09:44 +0900

    flont_index

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ApplicationRecord
   has_many :memo_tags
   has_many :memos, through: :memo_tags
   validates :name, presence: true, length: { maximum: 10 }
+  def self.ransackable_attributes(auth_object = nil)
+    ["name"]
+  end
 end
