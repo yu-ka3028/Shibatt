75d140d0d60c642f1fc203ce192e2fd3e034afdb
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-02 18:05:55 +0900

    debug2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       logger.info("Received ID token: #{id_token}")
       channel_id = "保存したチャネルIDを入れる"
       res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
+      logger.info("Response from LINE API: #{res.body}")
       line_user_id = JSON.parse(res.body)['sub']
       user = User.find_by(line_user_id: line_user_id)
       if user.nil?
