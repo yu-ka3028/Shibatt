b86d252c70a7e2f1fd3f673f59b4965a523f7dbe
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-02 20:17:00 +0900

    debug

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -16,5 +16,4 @@ Rails.application.config.sorcery.configure do |config|
   config.line.scope = 'profile'
   #LINEから取得したdisplayNameをローカルでusernameとして使用
   config.line.user_info_mapping = { username: 'displayName' }
-  config.line.user_info_mapping = { username: 'displayName' }
 end
