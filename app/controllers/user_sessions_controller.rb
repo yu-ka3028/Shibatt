e920454caf5dd4445c209a0c349dacd9223c61fc
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-03 08:10:27 +0900

    401

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,20 +2,21 @@ class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
   def create
-    @user = login(params[:user_session][:username], params[:user_session][:password])
-  
-    if @user
-      session[:username] = params[:user_session][:username]
-      session[:profileImageUrl] = params[:user_session][:profileImageUrl]
+    username = params[:username]
+    line_user_id = params[:line_user_id]
+    profile_image_url = params[:profile_image_url]
+
+    # ユーザー名が存在するか確認し、存在しない場合は新しいユーザーを作成
+    @user = User.find_by(username: username) || User.new(username: username, line_user_id: line_user_id)
+
+    if @user.save
+      auto_login(@user)
+      session[:username] = username
+      session[:profileImageUrl] = profile_image_url
       redirect_back_or_to root_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
       render :new, status: :unauthorized
     end
-  end
-
-  def destroy
-    logout
-    redirect_to root_path, notice: 'Logged out!'
   end
 end
