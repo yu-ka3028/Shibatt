c61c420b195cd5096fdf4c9b057a38593940d8b6
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-03 15:04:07 +0900

    bug3

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,6 +12,30 @@ class UserSessionsController < ApplicationController
     end
   end
 
+  def create_from_web
+    provider = params[:provider]
+    if @user = login_from(provider)
+      puts "Logged in from #{provider}"
+      redirect_to root_path, notice: "#{provider.titleize}からログインしました!"
+    else
+      begin
+        @user = User.find_by(line_user_id: @user.authentications.find_by(provider: provider).uid) || create_from(provider)
+        puts "Created user from #{provider}: #{@user.inspect}"
+        # LINEから取得したuserIdをローカルでline_user_idに保存
+        @user.update(line_user_id: @user.authentications.find_by(provider: provider).uid)
+        puts @user.line_user_id
+  
+        reset_session
+        auto_login(@user)
+    
+        redirect_to root_path, notice: "#{provider.titleize}からログインしました!"
+      rescue => e
+        puts "Failed to login from #{provider}: #{e.message}"
+        redirect_to root_path, alert: "#{provider.titleize}からのログインに失敗しました!"
+      end
+    end
+  end
+
   def create_from_liff
     username = params[:user_session][:username]
     line_user_id = params[:user_session][:line_user_id]
