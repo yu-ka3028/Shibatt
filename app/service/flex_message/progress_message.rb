af1d0cdc6b323b92c184d30fbc4fa8a9a8fa865f
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-08 19:32:24 +0900

    crash

--- app/service/flex_message/progress_message.rb
+++ app/service/flex_message/progress_message.rb
@@ -38,7 +38,7 @@ module FlexMessage
                 },
                 {
                   "type": "text",
-                  "text": "#{rates[:in progress]}%", # 未達成率を表示
+                  "text": "#{rates[:in_progress]}%" # 未達成率
                   "color": "#ffffff",
                   "align": "start",
                   "size": "xs",
