323b9f7da284beaab9286d73e062462da45dd984
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-11 08:06:34 +0900

    progress

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -14,33 +14,10 @@ class Memo < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     %w(tags)
   end
-
-  def progress_rate
-    total = memos.count
-    in_progress = memos.where(progress: 'in progress').count
-    completed = memos.where(progress: 'completed').count
-  
-    in_progress_rate = (in_progress.to_f / total * 100).round(2)
-    completed_rate = (completed.to_f / total * 100).round(2)
-  
-    { in_progress: in_progress_rate, completed: completed_rate }
-  end
   
   def memo_tags=(name)
     self.tags = [Tag.where(name: name.strip).first_or_create!]
   end
-  # def memo_tags(tags = "")
-  #   current_tags = self.tags.pluck(:name)
-  #   tags = tags.split(',') if tags.is_a?(String)
-  
-  #   tags.each do |tag_name|
-  #     tag = Tag.find_or_create_by(name: tag_name.strip)
-  #     self.tags << tag unless self.tags.include?(tag)
-  #   end
-  
-  #   tags_to_delete = current_tags - tags
-  #   self.tags.where(name: tags_to_delete).destroy_all
-  # end
 
   def tag_index
     self.tags.map(&:name).join(', ')
