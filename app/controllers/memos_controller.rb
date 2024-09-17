1842fa4ed53ac64444061e11ca2fa52a986507f4
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-09-17 13:44:20 +0900

    non_:memo_derete

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -105,6 +105,6 @@ class MemosController < ApplicationController
   private
 
   def memo_params
-    params.require(:memo).permit(:content, :progress, reflection_memo_ids: [], memo_tags: [])
+    params.fetch(:memo, {}).permit(:content, :progress, reflection_memo_ids: [], memo_tags: [])
   end
 end
