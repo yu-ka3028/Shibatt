c3367bba895a031ad835b082b0c6f00842de4129
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-01 09:15:06 +0900

    email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_06_023202) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_01_001442) do
   create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "provider", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_023202) do
     t.datetime "updated_at", null: false
     t.string "line_user_id"
     t.string "profile_image_url"
+    t.string "email"
     t.index ["line_user_id"], name: "index_users_on_line_user_id"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
