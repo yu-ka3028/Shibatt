cec921d016682873b40f02a244e2189b96c2582a
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-03 14:00:00 +0900

    uid2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_03_044830) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_03_045915) do
   create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "provider", null: false
-    t.string "uid", null: false
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "line_user_id"
