2d00ed2120b84eaad56ec72070c88a7fae52dbfd
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-15 17:53:33 +0900

    set

--- app/javascript/liff_set.js
+++ app/javascript/liff_set.js
@@ -1,38 +1,36 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  liff.init({ liffId: "2006024454-QgjEWevp" })
-  .then(() => {
-    return liff.getProfile();
-  })
-  .then(profile => {
-    // サーバーに POST リクエストを送信
-    fetch('/user_sessions/create_from_liff', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-      },
-      body: JSON.stringify({
-        user_session: {
-          username: profile.displayName,
-          profileImageUrl: profile.pictureUrl,
-          line_user_id: profile.userId
-        }
-      })
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+liff.init({ liffId: "2006024454-QgjEWevp" })
+.then(() => {
+  return liff.getProfile();
+})
+.then(profile => {
+  // サーバーに POST リクエストを送信
+  fetch('/user_sessions/create_from_liff', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+    },
+    body: JSON.stringify({
+      user_session: {
+        username: profile.displayName,
+        profileImageUrl: profile.pictureUrl,
+        line_user_id: profile.userId
       }
-      return response.json();
     })
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
   })
-  .catch((err) => {
-    console.error('LIFF Initialization failed ', err);
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
   });
+})
+.catch((err) => {
+  console.error('LIFF Initialization failed ', err);
 });
