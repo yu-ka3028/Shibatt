b216c2228b00cb712918375b7ab97f15b1b911cb
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-17 20:31:23 +0900

    logout_error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  accepts_nested_attributes_for :authentications
   has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
 
   has_many :memos, dependent: :destroy
   has_many :reflection_memos
