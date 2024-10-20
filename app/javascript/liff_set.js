4b6d395fd280b237b3a00b48f8abbd2c47f27a93
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-20 11:54:43 +0900

    edit/logout_liff3

--- app/javascript/liff_set.js
+++ app/javascript/liff_set.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  liff.init({ liffId: "2006024454-QgjEWevp" })
-  .then(() => {
-    return liff.getProfile();
-  })
-  .then(profile => {
-    // サーバーに POST リクエストを送信
-    fetch('/user_sessions/create_from_liff', {
+document.addEventListener('DOMContentLoaded', async (event) => {
+  try {
+    await liff.init({ liffId: "2006024454-QgjEWevp" });
+    const profile = await liff.getProfile();
+
+    const response = await fetch('/user_sessions/create_from_liff', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -18,21 +16,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
           line_user_id: profile.userId
         }
       })
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
     });
-  })
-  .catch((err) => {
-    console.error('LIFF Initialization failed ', err);
-  });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log('Success:', data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
